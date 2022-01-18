41702116368f518112ad7b948012d4969671a98b
kodluyoruz <baristutakli@hotmail.com>
2022-01-18 20:05:04 +0300

    Added middleware

--- WebApi/Controllers/BooksController.cs
+++ WebApi/Controllers/BooksController.cs
@@ -13,119 +13,102 @@ using AutoMapper;
 using FluentValidation.Results;
 using FluentValidation;
 
-namespace WebApi.AddControllers{
+namespace WebApi.AddControllers
+{
 
     [ApiController]
     [Route("api/[controller]")]
-    public class BooksController:Controller{
- 
+    public class BooksController : Controller
+    {
+
         private readonly BookStoreDbContext _context;
 
         private readonly IMapper _mapper;
 
         public BooksController(BookStoreDbContext context, IMapper mapper)
         {
-            _context=context;
+            _context = context;
             _mapper = mapper;
         }
         [HttpGet]
-        public IActionResult Get(){
-            GetBooksQuery query = new GetBooksQuery(_context,_mapper);
+        public IActionResult Get()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context, _mapper);
             var result = query.Handle();
-            if(result is null)
+            if (result is null)
                 return BadRequest();
             return Ok(result);
         }
-         [HttpGet("{id}")]
-        public IActionResult GetById(int id){
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
 
             BookDetailViewModel result;
-            
-            try
-            {
-                GetBookDetailQuery query = new GetBookDetailQuery(_context,_mapper);
-                GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
-               
-                query.BookId = id;
-                validator.ValidateAndThrow(query);
-                result = query.Handle();
-            }
-            catch (Exception ex)
-            {
 
-                return BadRequest(ex.Message);
-            }
-            
+
+            GetBookDetailQuery query = new GetBookDetailQuery(_context, _mapper);
+            GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
+
+            query.BookId = id;
+            validator.ValidateAndThrow(query);
+            result = query.Handle();
+
+
             return Ok(result);
         }
         [HttpPost]
-        public IActionResult AddBook([FromBody]CreateBookModel bookModel){
+        public IActionResult AddBook([FromBody] CreateBookModel bookModel)
+        {
             CreateBookCommand command = new CreateBookCommand(_context, _mapper);
-            try
-            {
-                command.Model = bookModel;
-                CreateBookCommandValidator validator = new CreateBookCommandValidator();
 
-                validator.ValidateAndThrow(command);
-                //if (!result.IsValid)
-                //{
-                //    foreach (var item in result.Errors)
-                //    {
-                //        Console.WriteLine("Özellik:"+item.PropertyName+" Erros:"+item.ErrorMessage);
-                //    }
-                //}
+            command.Model = bookModel;
+            CreateBookCommandValidator validator = new CreateBookCommandValidator();
+
+            validator.ValidateAndThrow(command);
+            //if (!result.IsValid)
+            //{
+            //    foreach (var item in result.Errors)
+            //    {
+            //        Console.WriteLine("Özellik:"+item.PropertyName+" Erros:"+item.ErrorMessage);
+            //    }
+            //}
 
 
-                command.Handle();
-            }
-            catch (Exception ex)
-            {
+            command.Handle();
 
-                return BadRequest(ex.Message);
-            }
-           
+
 
             return Ok();
         }
         [HttpPut("{id}")]
-        public IActionResult Update(int id,[FromBody] UpdateBookModel updatedBook){
-            try
-            {
-                UpdateBookCommand command = new UpdateBookCommand(_context);
-                UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+        public IActionResult Update(int id, [FromBody] UpdateBookModel updatedBook)
+        {
 
-                command.BookId = id;
-                command.Model = updatedBook;
-                validator.ValidateAndThrow(command);
-                command.Handle();
-            }
-            catch (Exception ex)
-            {
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
 
-                return BadRequest(ex.Message);
-            }
-            
+            command.BookId = id;
+            command.Model = updatedBook;
+            validator.ValidateAndThrow(command);
+            command.Handle();
+
+
 
             return Ok();
         }
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id){
-            
-            try
-            {
-                DeleteBookCommand command = new DeleteBookCommand(_context);
-                command.BookId = id;
-                DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
-                validator.ValidateAndThrow(command);
-                command.Handle();
-            }
-            catch (Exception ex)
-            {
+        public IActionResult Delete(int id)
+        {
 
-                return BadRequest(ex.Message);
-            }
+
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            command.BookId = id;
+            DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle();
+
             return Ok();
         }
-       
+
     }
 }
---
+++ WebApi/MiddleWares/CustomExceptionMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebApi.MidleWares
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+
+        public CustomExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var watch = Stopwatch.StartNew();
+            try
+            {
+                string message = "[Request]  HTTP-" + context.Request.Method + " - " + context.Request.Path;
+
+
+                await _next(context);
+                watch.Stop();
+
+                message = "[Response] HTTP-" + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms";
+
+            }
+            catch (Exception ex)
+            {
+                watch.Stop();
+                await HandleException(context, ex, watch);
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            string message = "[Error]   HTTP " + context.Request.Method + " - " + context.Response.StatusCode +
+                             "Error Message : " + ex.Message
+                             + " in " + watch.Elapsed.TotalMilliseconds;
+
+
+            var result = JsonConvert.SerializeObject(new { error = ex.Message });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+    public static class CustomExceptionMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomExceptionMiddle(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionMiddleware>();
+        }
+    }
+}
